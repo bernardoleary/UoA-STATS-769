2a8dc5479d2bad1c800ee1bd464565867c1d4c5f
Bernard <bernard.oleary@gmail.com>
2019-10-06 18:33:09 +1300

    Changed RMD

--- Labs/Lab08/STATS769_2019_S2_bole001_lab08.sh
+++ Labs/Lab08/STATS769_2019_S2_bole001_lab08.sh
@@ -3,7 +3,7 @@
 
 # Run the R code for the lab
 echo "Running RMD script"
-Rscript -e 'library("rmarkdown"); render("STATS769_2019_S2_bole001_lab08.Rmd")'
+/usr/bin/time -f "%e" Rscript -e 'library("rmarkdown"); render("STATS769_2019_S2_bole001_lab08.Rmd")'
 
 # Exit
 echo "Done"
